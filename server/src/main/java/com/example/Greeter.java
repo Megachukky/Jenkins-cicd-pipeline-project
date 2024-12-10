1ec6735dfd0c7b3c1873f865f6826a3c42567da5
Megachukky <megachukky@gmail.com>
2024-12-10 11:08:33 -0500

    modify

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,17 +9,13 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-    // Empty constructor
+
   }
 
   /**
    * This is a method.
-   * 
-   * @param someone the name to greet
-   * @return a greeting message
    */
   public final String greet(final String someone) {
-    // TODO: Remove hardcoded credentials before production
     // String DB_PASSWORD = "Admin@1234";
     return String.format("Hello Avinash, %s!", someone);
   }
